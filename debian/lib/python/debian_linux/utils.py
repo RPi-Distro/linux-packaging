12e57e1c210909b624154fe4fe9bc472f3c3b7c3
Bastian Blank <waldi@debian.org>
2022-12-02 20:10:12 +0100

    Support Jinja2 templates
    Several of our templates use two template mechanisms: our template
    substitution and dh-make.  Primarily lintian overrides use dh-make to
    support architecture selection.
    
    Jinja2 is a well known template language.  In addition to text
    substitution, it also supports simple code and conditions.  This allows
    to do things like architecture selection in one place.

--- debian/lib/python/debian_linux/utils.py
+++ debian/lib/python/debian_linux/utils.py
@@ -4,22 +4,32 @@ import re
 import textwrap
 import typing
 
+import jinja2
+
 from .debian import SourcePackage, BinaryPackage, TestsControl
 
 
 class Templates(object):
     dirs: list[str]
     _cache: dict[str, str]
+    _jinja2: jinja2.Environment
 
     def __init__(self, dirs: list[str] = ["debian/templates"]) -> None:
         self.dirs = dirs
 
         self._cache = {}
+        self._jinja2 = jinja2.Environment(
+            # autoescape uses HTML safe escaping, which does not help us
+            autoescape=False,
+            keep_trailing_newline=True,
+            trim_blocks=True,
+            undefined=jinja2.StrictUndefined,
+        )
 
     def _read(self, name: str) -> typing.Any:
         pkgid, name = name.rsplit('.', 1)
 
-        for suffix in ['.in', '']:
+        for suffix in ['.j2', '.in', '']:
             for dir in self.dirs:
                 filename = "%s/%s.%s%s" % (dir, pkgid, name, suffix)
                 if os.path.exists(filename):
@@ -45,6 +55,9 @@ class Templates(object):
                 def subst(match):
                     return context[match.group(1)]
                 return re.sub(r'@([-_a-z0-9]+)@', subst, str(value[0]))
+
+            elif suffix == '.j2':
+                return self._jinja2.from_string(value[0]).render(context)
 
         return value[0]
 
